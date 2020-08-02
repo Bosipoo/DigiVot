30ded621ee89f59b774ef12c58ab7dfefe7ea9d8
Ajawu David <ajawudavid@gmail.com>
2020-08-02 23:37:57 +0100

    removed virtual environment and migration files

--- nenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\bosipo\documents\final year project\frontend\digivot\nenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
